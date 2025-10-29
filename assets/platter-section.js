dc9da07b3f3469ac6596ee83429a1d2c17179020
Isaac Alvarez <isaacalvarez@Mac.fios-router.home>
2025-10-29 12:32:26 -0400

    adds smooth dropdown for mobile feature

--- assets/platter-section.js
+++ assets/platter-section.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const viewAllButton = document.querySelector('[data-view-all-button]');
   const productContainer = document.querySelector('[data-display-first-four]');
-  
-  if (viewAllButton && productContainer) {
-    viewAllButton.addEventListener('click', () => {
-      const hiddenSlides = productContainer.querySelectorAll('.mobile-view-more:nth-child(n + 5)');
 
-      hiddenSlides.forEach(slide => {
-        slide.style.display = 'block';
-      });
-      viewAllButton.style.display = 'none';
+  if (!viewAllButton || !productContainer) return;
+
+  viewAllButton.addEventListener('click', () => {
+    const hiddenSlides = productContainer.querySelectorAll('.mobile-view-more:nth-child(n + 5)');
+
+    hiddenSlides.forEach((slide) => {
+      slide.style.display = 'block';
+      const height = slide.scrollHeight;
+      slide.style.setProperty('--target-height', `${height}px`);
+      slide.classList.add('open');
     });
-  }
+    viewAllButton.style.display = 'none';
+  });
 });
